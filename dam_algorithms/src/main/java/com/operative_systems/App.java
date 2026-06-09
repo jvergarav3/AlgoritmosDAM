622ab81712fbe407dc086346828a1ac541394218
joan566 <joansebastianvergaravalencia@gmail.com>
2026-06-09 15:36:24 -0500

    Creacion de la clase Principal App

---
+++ dam_algorithms/src/main/java/com/operative_systems/App.java
@@ -0,0 +1,13 @@
+package com.operative_systems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
